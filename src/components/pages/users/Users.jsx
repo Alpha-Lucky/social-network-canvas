f59c1661db7aef5a35545037df8c4b9426a9c486
Alpha-Lucky <aplha.lucky.m@gmail.com>
2021-09-12 11:52:06 +0300

    Axios

--- src/components/pages/users/Users.jsx
+++ src/components/pages/users/Users.jsx
@@ -1,14 +1,20 @@
+import * as axios from 'axios';
+import ava from '../../img/avaDefolt.jpg'
 import React from 'react';
 import './users.css'
 
 let Users = (props) => {
-
+    if(props.users.length === 0) {
+      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        props.setUsers(response.data.items)
+      })
+    }
 
   return <div className="friendsContainer">
     {
       props.users.map(u => <div className="friendsContainer__eUser" kay={u.id}>
         <div className="eUser__ava">
-          <img src={u.urlImg} alt={u.name} />
+          <img src={ u.photos.small != null ? u.photos.small : ava } alt={u.id} />
           {u.followed 
            ? <button onClick={() => { props.unFollow(u.id) }}>Unfollow</button>
            : <button onClick={() => { props.follow(u.id) }}>follow</button>
@@ -24,7 +30,7 @@ let Users = (props) => {
           </div>
 
           <div className="description__location">
-            <span>{u.location.city}</span>
+            <span>{/* u.location.city */}</span>
           </div>
         </div>
       </div>)
